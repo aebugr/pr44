1c00d0b573f3697cfc0766890905b00ac5be1fff
BugrinaA <tsaaeel@gmail.com>
2024-05-30 23:05:27 +0500

    Разработанная модель представления главного окна. Бугрина

---
+++ ViewModels/VM_Pages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManagerBugrina.Classes;
+
+namespace TaskManagerBugrina.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Tasks vm_tasks = new VM_Tasks();
+        public VM_Pages() => MainWindow.init.frame.Navigate(new View.Main(vm_tasks));
+        public RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}
