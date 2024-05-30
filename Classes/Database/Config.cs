e4e1a0aa1e75ff25ef0651172cbb2e23c46c64b4
BugrinaA <tsaaeel@gmail.com>
2024-05-30 22:28:48 +0500

    Созданный класс Config. Бугрина

---
+++ Classes/Database/Config.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagerBugrina.Classes.Database
+{
+    public class Config
+    {
+        public static readonly string connection = "server=127.0.0.1; " +
+            "port=3312;" +
+            "uid=root;" +
+            "pwd=;" +
+            "database=TaskManager;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
